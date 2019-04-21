3c7f3414c97005ef30e9efb8d27c8ee2b69945a3
dung668 <nddungx@gmail.com>
2019-04-21 16:30:43 +0700

    change sql, frontend

--- db/tax_sqa.sql
+++ db/tax_sqa.sql
@@ -16,30 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `company_info`
---
-
-DROP TABLE IF EXISTS `company_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `company_info` (
-  `ma_cong_ty` int(11) NOT NULL AUTO_INCREMENT,
-  `ten_cong_ty` varchar(100) NOT NULL,
-  `dia_chi` varchar(45) NOT NULL,
-  PRIMARY KEY (`ma_cong_ty`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `company_info`
---
-
-LOCK TABLES `company_info` WRITE;
-/*!40000 ALTER TABLE `company_info` DISABLE KEYS */;
-/*!40000 ALTER TABLE `company_info` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `tax_config`
 --
 
@@ -50,7 +26,7 @@ CREATE TABLE `tax_config` (
   `ma_cau_hinh` int(11) NOT NULL AUTO_INCREMENT,
   `ban_than` int(11) NOT NULL,
   `nguoi_phu_thuoc` int(11) NOT NULL,
-  `bao_hiem` int(11) NOT NULL,
+  `tien_an` int(11) NOT NULL,
   PRIMARY KEY (`ma_cau_hinh`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -72,24 +48,19 @@ DROP TABLE IF EXISTS `user_tax`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `user_tax` (
-  `cmt` varchar(15) NOT NULL,
+  `id` varchar(15) NOT NULL,
   `ho_ten` varchar(50) NOT NULL,
-  `ma_cong_ty` int(11) NOT NULL,
   `chuc_vu` varchar(20) NOT NULL,
   `mst` varchar(45) NOT NULL,
-  `luong` float NOT NULL,
-  `tien_an` varchar(45) NOT NULL,
+  `luong` int(11) NOT NULL,
+  `tien_an` int(11) NOT NULL,
+  `giam_tru_ban_than` int(11) NOT NULL,
+  `giam_tru_phu_thuoc` int(11) NOT NULL,
   `so_nguoi_phu_thuoc` int(11) NOT NULL,
-  `giam_tru_gia_canh` int(11) NOT NULL,
-  `thu_nhap_chiu_thue` int(11) NOT NULL,
   `thue_phai_nop` int(11) NOT NULL,
-  `ma_cau_hinh_thue` int(11) NOT NULL,
-  PRIMARY KEY (`cmt`),
-  UNIQUE KEY `cmt_UNIQUE` (`cmt`),
-  KEY `fk_company_info_idx` (`ma_cong_ty`),
-  KEY `fk_tax_config_idx` (`ma_cau_hinh_thue`),
-  CONSTRAINT `fk_company_info` FOREIGN KEY (`ma_cong_ty`) REFERENCES `company_info` (`ma_cong_ty`),
-  CONSTRAINT `fk_tax_config` FOREIGN KEY (`ma_cau_hinh_thue`) REFERENCES `tax_config` (`ma_cau_hinh`)
+  `thoi_gian_chiu_thue` date NOT NULL,
+  `ngay_tao_hoa_don` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -111,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-11 23:02:20
+-- Dump completed on 2019-04-21 16:29:08
